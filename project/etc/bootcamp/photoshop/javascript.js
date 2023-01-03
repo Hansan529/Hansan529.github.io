d28b33c0c934d150aa1bcda0b2a6d5983ce413f9
hansan0529 <hansan0529@gmail.com>
2023-01-03 17:49:44 +0900

    "" -> ''

--- project/etc/bootcamp/photoshop/javascript.js
+++ project/etc/bootcamp/photoshop/javascript.js
@@ -1,12 +1,10 @@
-const mouseHover = Array.from(document.getElementsByClassName("dp1"));
-const header = document.getElementById("header");
-const overlap = document.getElementById("overlap");
+const mouseHover = Array.from(document.getElementsByClassName('dp1'));
+const header = document.getElementById('header');
+const overlap = document.getElementById('overlap');
 
-function alpha() {
-
-}
+function alpha() {}
 
 function beta() {}
 
-mouseHover.forEach((header) => header.addEventListener("mouseover", alpha));
-overlap.addEventListener("mouseleave", beta);
+mouseHover.forEach((header) => header.addEventListener('mouseover', alpha));
+overlap.addEventListener('mouseleave', beta);
