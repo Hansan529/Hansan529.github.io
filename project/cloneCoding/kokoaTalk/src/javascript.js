82aac78830227ba70274c3bbe60661f905587021
hansan0529 <hansan0529@gmail.com>
2023-01-03 17:50:03 +0900

    주석 제거

--- project/cloneCoding/kokoaTalk/src/javascript.js
+++ project/cloneCoding/kokoaTalk/src/javascript.js
@@ -1,18 +1,14 @@
-// const loginInfo = document.getElementById("login__keep_info");
-const clock = document.querySelector("#status-bar__time");
-
+const clock = document.querySelector('#status-bar__time');
 
 const getClock = () => {
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
+   const date = new Date();
+   const hours = String(date.getHours()).padStart(2, '0');
+   const minutes = String(date.getMinutes()).padStart(2, '0');
 
-  clock.innerText = `${hours} : ${minutes}`;
+   clock.innerText = `${hours} : ${minutes}`;
 };
 
 getClock();
 setInterval(getClock, 1000);
 
-// loginInfo.addEventListener("click", );
-
-
+const alpha = document.getElementsByClassName('nav__notification');
