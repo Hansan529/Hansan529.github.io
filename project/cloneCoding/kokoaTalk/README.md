7f7d7996babb509174d7431e5f86a83888c72bd4
hansan529 <hansan0529@gmail.com>
2023-01-04 11:57:54 +0900

    repository 분리 작업

--- project/cloneCoding/kokoaTalk/README.md
+++
@@ -1,3 +0,0 @@
-# Kokoa Clone coding
-
-[html과 css markup으로 만드는 코코아톡 클론코딩](https://hansan529.github.io/project/cloneCoding/kokoaTalk/index.html)
--- project/cloneCoding/kokoaTalk/src/chats.js
+++
@@ -1,9 +0,0 @@
-const submitMessage = document.getElementById("chats-input__submit-btn");
-
-function submit(event) {
-  event.preventDefault();
-  console.log("dd");
-  
-}
-
-submitMessage.addEventListener("submit", submit);
